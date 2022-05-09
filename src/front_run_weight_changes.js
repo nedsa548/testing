57d8323ef50a99f783f5af3260f984cc4b525416
nedsa548 <78696251+nedsa548@users.noreply.github.com>
2022-05-09 21:05:13 +0300

    Update front_run_weight_changes.js

--- src/front_run_weight_changes.js
+++ src/front_run_weight_changes.js
@@ -1,7 +1,11 @@
 require("dotenv").config({path: "../.env"});
 const CONFIG = require("../config/config.js");
 const Web3 = require("web3");
-//const ethereumWeb3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3"));
+
+//  ---------------------------------------- RPCs ----------------------------------------
+//  RPC URLs for web3 calls.
+
+//const ethereumWeb3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3")); **The url doesn't work.
 const avalancheWeb3 = new Web3(new Web3.providers.HttpProvider("https://api.avax.network/ext/bc/C/rpc"));
 const bscWeb3 = new Web3(new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org"));
 const polygonWeb3 = new Web3(new Web3.providers.HttpProvider("https://polygon-rpc.com"));
@@ -15,14 +19,17 @@ const bobaWeb3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.boba.
 const kccWeb3 = new Web3(new Web3.providers.HttpProvider("https://rpc-mainnet.kcc.network"));
 const metisWeb3 = new Web3(new Web3.providers.HttpProvider("https://www.andromeda.metis.io/?owner=1088"));
 
-const { Telegraf } = require("telegraf");
+// -------------------------------------- Telegram --------------------------------------
 
+const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.WEIGHT_CHANGES_BOT_TG_TOKEN);
 const user_ids = CONFIG.WEIGHT_CHANGES_BOT.TG_USER_ID;
 bot.start((ctx) => ctx.reply("Welcome."));
 bot.launch();
 
-//const sushiEthereumABI = require("../abis/weight_changes/sushiEthereumABI.json");
+//  ---------------------------------------- ABIs ----------------------------------------
+
+const sushiEthereumABI = require("../abis/weight_changes/sushiEthereumABI.json");
 const traderJoeABI = require("../abis/weight_changes/traderJoeABI.json");
 const pangolinABI = require("../abis/weight_changes/pangolinABI.json");
 const pancakeswapABI = require("../abis/weight_changes/pancakeswapABI.json");
@@ -44,32 +51,36 @@ const oolongswapABI = require("../abis/weight_changes/oolongswapABI.json");
 const mojitoswapABI = require("../abis/weight_changes/mojitoswapABI.json");
 const netswapABI = require("../abis/weight_changes/netswapABI.json");
 
+//  --------------------------------------------------------------------------------------
+
 const dexes = [
-    //name, chain, address(mini/masterchef), ABI, isRewarder, swap url, analytics url, twitter
-    //{ name: "Sushiswap(Ethereum)", chain: "ethereum", contract: "0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d",  abi: sushiEthereumABI, additionalCall: true, swapLink: "https://app.sushi.com/trident/swap?chainId=1", analyticsLink: "", twittter: "SushiSwap" },
-    { name: "Trader Joe", chain: "avalanche", contract: "0x188bED1968b795d5c9022F6a0bb5931Ac4c18F00", abi: traderJoeABI, additionalCall: false, swapLink: "https://traderjoexyz.com/trade", analyticsLink: "https://analytics.traderjoexyz.com/pairs/", twittter: "traderjoe_xyz" },
-    { name: "Pangolin", chain: "avalanche", contract: "0x1f806f7C8dED893fd3caE279191ad7Aa3798E928", abi: pangolinABI, additionalCall: true, swapLink: "https://app.pangolin.exchange/#/swap", analyticsLink: "https://info.pangolin.exchange/#/pair/", twittter: "pangolindex" },
-    { name: "Pancakeswap", chain: "bsc", contract: "0x73feaa1eE314F8c655E354234017bE2193C9E24E", abi: pancakeswapABI, additionalCall: false, swapLink: "https://pancakeswap.finance/swap", analyticsLink: "https://pancakeswap.finance/info/pool/", twittter: "PancakeSwap" },
-    { name: "Biswap", chain: "bsc", contract: "0xDbc1A13490deeF9c3C12b44FE77b503c1B061739", abi: biswapABI, additionalCall: false, swapLink: "https://exchange.biswap.org/#/swap", analyticsLink: "https://biswap.org/analytics/pool/", twittter: "biswap_dex" },
-    { name: "Apeswap(BSC)", chain: "bsc", contract: "0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9", abi: apeswapBscABI, additionalCall: false, swapLink: "https://apeswap.finance/swap", analyticsLink: "https://info.apeswap.finance/pair/", twittter: "ape_swap" },
-    { name: "Sushiswap(Polygon)", chain: "polygon", contract: "0x0769fd68dFb93167989C6f7254cd0D766Fb2841F", abi: sushiPolygonABI, additionalCall: true, swapLink: "https://app.sushi.com/trident/swap?chainId=137", analyticsLink: "", twittter: "SushiSwap" },
-    { name: "Apeswap(Polygon)", chain: "polygon", contract: "0x54aff400858Dcac39797a81894D9920f16972D1D", abi: apeswapPolygonABI, additionalCall: false, swapLink: "https://apeswap.finance/swap", analyticsLink: "", twittter: "ape_swap" },
-    { name: "Spookyswap", chain: "fantom", contract: "0x18b4f774fdC7BF685daeeF66c2990b1dDd9ea6aD", abi: spookyswapABI, additionalCall: true, swapLink: "https://spookyswap.finance/swap", analyticsLink: "https://info.spookyswap.finance/pair/", twittter: "SpookySwap" },
-    { name: "Spiritswap", chain: "fantom", contract: "0x9083EA3756BDE6Ee6f27a6e996806FBD37F6F093", abi: spiritswapABI, additionalCall: false, swapLink: "https://swap.spiritswap.finance/#/exchange/swap", analyticsLink: "https://info.spiritswap.finance/pair/", twittter: "spirit_swap" },
-    { name: "Defi Kingdoms", chain: "harmony", contract: "0xDB30643c71aC9e2122cA0341ED77d09D5f99F924", abi: dfkABI, additionalCall: false, swapLink: "https://game.defikingdoms.com/", analyticsLink: "", twittter: "DefiKingdoms" },
-    { name: "Viperswap", chain: "harmony", contract: "0x7AbC67c8D4b248A38B0dc5756300630108Cb48b4", abi: viperswapABI, additionalCall: false, swapLink: "https://game.defikingdoms.com/", analyticsLink: "", twittter: "DefiKingdoms" },
-    { name: "Mad Meerkat Finance", chain: "cronos", contract: "0x6bE34986Fdd1A91e4634eb6b9F8017439b7b5EDc", abi: mmFinanceABI, additionalCall: false, swapLink: "https://mm.finance/swap/", analyticsLink: "", twittter: "mmfcrypto" },
-    { name: "VVS Finance", chain: "cronos", contract: "0xDccd6455AE04b03d785F12196B492b18129564bc", abi: vvsFinanceABI, additionalCall: false, swapLink: "https://vvs.finance/swap/", analyticsLink: "https://vvs.finance/info/farm/", twittter: "vvs_finance" },
-    { name: "Trisolaris", chain: "aurora", contract: "0x3838956710bcc9D122Dd23863a0549ca8D5675D6", abi: trisolarisABI, additionalCall: true, swapLink: "https://www.trisolaris.io/#/swap", analyticsLink: "", twittter: "trisolarislabs" },
-    { name: "Solarbeam", chain: "moonriver", contract: "0xf03b75831397D4695a6b9dDdEEA0E578faa30907", abi: solarbeamABI, additionalCall: false, swapLink: "https://app.solarbeam.io/exchange/swap", analyticsLink: "https://analytics.solarbeam.io/pairs/", twittter: "solarbeamio" },
-    { name: "Stellaswap", chain: "moonbeam", contract: "0xEDFB330F5FA216C9D2039B99C8cE9dA85Ea91c1E", abi: stellaswapABI, additionalCall: false, swapLink: "https://app.stellaswap.com/exchange/swap", analyticsLink: "https://analytics.stellaswap.com/pairs/", twittter: "stellaswap" },
-    { name: "Beamswap", chain: "moonbeam", contract: "0xC6ca172FC8BDB803c5e12731109744fb0200587b", abi: beamswapABI, additionalCall: false, swapLink: "https://app.beamswap.io/exchange/swap", analyticsLink: "https://analytics.beamswap.io/pairs/", twittter: "beamswapio" },
-    { name: "Oolongswap", chain: "boba", contract: "0x31B9FBd965397d697D2dAa434EbD219aB878E49B", abi: oolongswapABI, additionalCall: false, swapLink: "https://oolongswap.com/#/swap", analyticsLink: "https://info.oolongswap.com/#/pair/", twittter: "oolongswap" },
-    { name: "Mojitoswap", chain: "kcc", contract: "0x25C6d6A65C3ae5d41599Ba2211629B24604Fea4F", abi: mojitoswapABI, additionalCall: false, swapLink: "https://app.mojitoswap.finance/swap", analyticsLink: "https://info.mojitoswap.finance/pair/", twittter: "mojitoswap" },
-    //{ name: "Netswap", chain: "metis", contract: "0x9d1dbB49b2744A1555EDbF1708D64dC71B0CB052", abi: netswapABI, additionalCall: false, swapLink: "https://netswap.io/#/swap", analyticsLink: "https://info.netswap.io/pair/", twittter: "netswapofficial" },
+    //name, chain, address(mini/masterchef), ABI, additionalCall(if there is a separate method for getting rewarder and lpToken info), twitter handle
+
+    //{ name: "Sushiswap(Ethereum)", chain: "ethereum", contract: "0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d",  abi: sushiEthereumABI, additionalCall: true, twittter: "SushiSwap" },
+    { name: "Trader Joe", chain: "avalanche", contract: "0x188bED1968b795d5c9022F6a0bb5931Ac4c18F00", abi: traderJoeABI, additionalCall: false, twittter: "traderjoe_xyz" },
+    { name: "Pangolin", chain: "avalanche", contract: "0x1f806f7C8dED893fd3caE279191ad7Aa3798E928", abi: pangolinABI, additionalCall: true, twittter: "pangolindex" },
+    { name: "Pancakeswap", chain: "bsc", contract: "0x73feaa1eE314F8c655E354234017bE2193C9E24E", abi: pancakeswapABI, additionalCall: false, twittter: "PancakeSwap" },
+    { name: "Biswap", chain: "bsc", contract: "0xDbc1A13490deeF9c3C12b44FE77b503c1B061739", abi: biswapABI, additionalCall: false, twittter: "biswap_dex" },
+    { name: "Apeswap(BSC)", chain: "bsc", contract: "0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9", abi: apeswapBscABI, additionalCall: false, twittter: "ape_swap" },
+    { name: "Sushiswap(Polygon)", chain: "polygon", contract: "0x0769fd68dFb93167989C6f7254cd0D766Fb2841F", abi: sushiPolygonABI, additionalCall: true, twittter: "SushiSwap" },
+    { name: "Apeswap(Polygon)", chain: "polygon", contract: "0x54aff400858Dcac39797a81894D9920f16972D1D", abi: apeswapPolygonABI, additionalCall: false, twittter: "ape_swap" },
+    { name: "Spookyswap", chain: "fantom", contract: "0x18b4f774fdC7BF685daeeF66c2990b1dDd9ea6aD", abi: spookyswapABI, additionalCall: true, twittter: "SpookySwap" },
+    { name: "Spiritswap", chain: "fantom", contract: "0x9083EA3756BDE6Ee6f27a6e996806FBD37F6F093", abi: spiritswapABI, additionalCall: false, twittter: "spirit_swap" },
+    { name: "Defi Kingdoms", chain: "harmony", contract: "0xDB30643c71aC9e2122cA0341ED77d09D5f99F924", abi: dfkABI, additionalCall: false, twittter: "DefiKingdoms" },
+    { name: "Viperswap", chain: "harmony", contract: "0x7AbC67c8D4b248A38B0dc5756300630108Cb48b4", abi: viperswapABI, additionalCall: false, twittter: "VenomDAO" },
+    { name: "Mad Meerkat Finance", chain: "cronos", contract: "0x6bE34986Fdd1A91e4634eb6b9F8017439b7b5EDc", abi: mmFinanceABI, additionalCall: false, twittter: "mmfcrypto" },
+    { name: "VVS Finance", chain: "cronos", contract: "0xDccd6455AE04b03d785F12196B492b18129564bc", abi: vvsFinanceABI, additionalCall: false, twittter: "vvs_finance" },
+    { name: "Trisolaris", chain: "aurora", contract: "0x3838956710bcc9D122Dd23863a0549ca8D5675D6", abi: trisolarisABI, additionalCall: true,  twittter: "trisolarislabs" },
+    { name: "Solarbeam", chain: "moonriver", contract: "0xf03b75831397D4695a6b9dDdEEA0E578faa30907", abi: solarbeamABI, additionalCall: false, twittter: "solarbeamio" },
+    { name: "Stellaswap", chain: "moonbeam", contract: "0xEDFB330F5FA216C9D2039B99C8cE9dA85Ea91c1E", abi: stellaswapABI, additionalCall: false, twittter: "stellaswap" },
+    { name: "Beamswap", chain: "moonbeam", contract: "0xC6ca172FC8BDB803c5e12731109744fb0200587b", abi: beamswapABI, additionalCall: false, twittter: "beamswapio" },
+    { name: "Oolongswap", chain: "boba", contract: "0x31B9FBd965397d697D2dAa434EbD219aB878E49B", abi: oolongswapABI, additionalCall: false, twittter: "oolongswap" },
+    { name: "Mojitoswap", chain: "kcc", contract: "0x25C6d6A65C3ae5d41599Ba2211629B24604Fea4F", abi: mojitoswapABI, additionalCall: false, twittter: "mojitoswap" },
+    //{ name: "Netswap", chain: "metis", contract: "0x9d1dbB49b2744A1555EDbF1708D64dC71B0CB052", abi: netswapABI, additionalCall: false, twittter: "netswapofficial" },
 ];
 
-async function checkNewFarms(pools, dex) {
+async function checkPools(pools, dex) { // compares pools to see if there is any change
+
     const knownPoolCount = Object.keys(pools).length;
     const currentPools = await getPools(dex);
     const currentPoolCount = Object.keys(currentPools).length;
@@ -81,7 +92,7 @@ async function checkNewFarms(pools, dex) {
 
     for (let pid = 0; pid < currentPoolCount; pid++) {
         if (pools[pid]) {
-            if (currentPools[pid].allocPoint !== pools[pid].allocPoint) {
+            if (currentPools[pid].allocPoint !== pools[pid].allocPoint) { // check if the farm weight has been changed. 
                 console.log(`Alloc point for this pool changed: ${(currentPools[pid].lpToken).toLowerCase()}`);
                 for (i in user_ids) {
                     bot.telegram.sendMessage(user_ids[i], `Alloc point for this farm changed on ${dex.name}: ${(currentPools[pid].lpToken).toLowerCase()} from ${pools[pid].allocPoint} to ${currentPools[pid].allocPoint}`);
@@ -89,15 +100,14 @@ async function checkNewFarms(pools, dex) {
                 return true;
             }
 
-            if (currentPools[pid].rewarder !== pools[pid].rewarder) {
+            if (currentPools[pid].rewarder !== pools[pid].rewarder) { // check if the pool's rewarder has been changed.
                 console.log(`Rewarder for this pool changed: ${(currentPools[pid].lpToken).toLowerCase()}`);
                 for (i in user_ids) {
                     bot.telegram.sendMessage(user_ids[i], `Rewarder for this farm changed on ${dex.name}: ${(currentPools[pid].lpToken).toLowerCase()} from ${pools[pid].rewarder} to ${currentPools[pid].rewarder}`);
                 }
                 return true;
             }
-        } else {
-            // Handle new pool
+        } else { //there is a new pool
             pools[pid] = currentPools[i];
             for (i in user_ids) {
                 bot.telegram.sendMessage(user_ids[i], `New farm found on ${dex.name} for this pool: ${(currentPools[pid].lpToken).toLowerCase()}. Analytics: https://dexscreener.com/${dex.chain}/${currentPools[pid].lpToken.toLowerCase()}`);
@@ -111,9 +121,10 @@ async function checkNewFarms(pools, dex) {
 }
 
 async function getPools(dex) {
-    const pools = {};
+    const pools = {}; // pools in a dex
     var contract = "";
 
+    //check what chain the dex is on so that we know which RPC to use for the web 3 contract.
     if (String(dex.chain) == "ethereum") {
         contract = new ethereumWeb3.eth.Contract(dex.abi, dex.contract);
     } else if (String(dex.chain) == "avalanche") {
@@ -144,6 +155,8 @@ async function getPools(dex) {
 
     const poolCount = parseInt(await contract.methods.poolLength().call());
 
+     // check if there is a separate method for getting rewarder and lpToken info. if there is, call them to get the info.
+
     if (!dex.additionalCall) {
         for (let pid = 0; pid < poolCount; pid++) {
             pools[pid] = await contract.methods.poolInfo(pid).call();
@@ -160,12 +173,12 @@ async function getPools(dex) {
 }
 
 async function getAllPools() {
-    var allPools = [];
-    var pools = {};
+    var allPools = []; //an array of lists
+    var pools = {}; // pools in a dex
     var contract = "";
 
     for (i in dexes) {
-        console.log(dexes[i].name);
+        //check what chain the dex is on so that we know which RPC to use for the web 3 contract.
         if (String(dexes[i].chain) == "ethereum") {
             contract = new ethereumWeb3.eth.Contract(dexes[i].abi, dexes[i].contract);
         } else if (String(dexes[i].chain) == "avalanche") {
@@ -193,9 +206,9 @@ async function getAllPools() {
         } else if (String(dexes[i].chain) == "metis") {
             contract = new metisWeb3.eth.Contract(dexes[i].abi, dexes[i].contract);
         }
-        const poolCount = parseInt(await contract.methods.poolLength().call());
-        console.log(poolCount);
+        const poolCount = parseInt(await contract.methods.poolLength().call()); // total pool count on the dex
 
+        // check if there is a separate method for getting rewarder and lpToken info. if there is, call them to get the info.
         if (!dexes[i].additionalCall) {
             for (let pid = 0; pid < poolCount; pid++) {
                 pools[pid] = await contract.methods.poolInfo(pid).call();
@@ -207,23 +220,24 @@ async function getAllPools() {
                 pools[pid]["lpToken"] = await contract.methods.lpToken(pid).call();
             }
         }
-        allPools.push(pools);
-        pools = {};
+
+        allPools.push(pools); // push the pool list(farms that the dex has)
+        pools = {}; // clear the list
     }
     return allPools;
 }
 
-async function checkPoolInfos() {
-    var weightChange = false;
+async function main() {
+    var isChange = false;
 
-    // Initialize current state of all pools
-    const allPools = await getAllPools();
+    const allPools = await getAllPools(); // returns a list of list of pools for each dex, ordered the same as dexes
 
-    while (!weightChange) {
-        for (i in dexes) {
-            weightChange = await checkNewFarms(allPools[i], dexes[i]);
+    while (!isChange) {
+        for (let i = 0; i < dexes.length; i++) {
+            console.log(i);
+            isChange = await checkPools(allPools[i], dexes[i]); //returns true if there is a new change(weight, rewarder contract) on a pool or if there is a new farm.
         }
     }
 }
 
-checkPoolInfos();
+main();
